320f7ead3213c1b4a9a85cfb963c725c71a7deed
cadelac <cadelac@github.com>
2018-06-25 14:33:55 +0800

    add map of web socket connections; log message with count of connections

--- src/main/java/cadelac/framework/pubsub/websocket/WebsocketServerConfig.java
+++ src/main/java/cadelac/framework/pubsub/websocket/WebsocketServerConfig.java
@@ -1,5 +1,9 @@
 package cadelac.framework.pubsub.websocket;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.websocket.CloseReason;
 import javax.websocket.Session;
 
@@ -12,6 +16,18 @@ import org.apache.log4j.Logger;
  */
 public class WebsocketServerConfig extends WebsocketConfig {
 	
+	public static class Lookup {
+
+		public Lookup(final Session session_) {
+			_session = session_;
+		}
+		public Session getSession() {
+			return _session;
+		}
+
+		private Session _session;
+	}
+	
 	
 	protected WebsocketServerConfig(final String id_) {
 		super(id_);
@@ -20,10 +36,14 @@ public class WebsocketServerConfig extends WebsocketConfig {
 	
 	protected void onOpenPrologue(final Session websocketSession_) 
 			throws Exception {
+		
+		final int count = put(websocketSession_);
+		
 		logger.info(String.format(
-				"A client just connected to us (server:%s): "
-					+ "websocket session [%s]"
+				"A client websocket session [%s] " 
+				+ "just connected (total=%d) to us (server:%s): "
 				, getId()
+				, count
 				, websocketSession_.getId()));
 	}
 	
@@ -32,14 +52,46 @@ public class WebsocketServerConfig extends WebsocketConfig {
 			final Session websocketSession_
 			, final CloseReason closeReason_) 
 					throws Exception {
+		
+		final int count = remove(websocketSession_);
+		
 		logger.info(String.format(
-				"A Websocket client disconnected from us (server:%s): " 
-						+ "websocket session [%s], reason [%s]"
+				"A websocket client session [%s] "
+				+ "disconnected (total=%d) from us (server:%s): "
+				+ "reason [%s]" 
 				, getId()
+				, count
 				, websocketSession_.getId()
 				, closeReason_.getReasonPhrase()));
 	}
+	
+	
+	private int put(final Session websocketSession_) {
+		LOOKUP_MAP.put(
+				websocketSession_.getId()
+				, new Lookup(websocketSession_));
+		return LOOKUP_MAP.size();
+	}
+	
+	public int remove(final Session websocketSession_) {
+		LOOKUP_MAP.remove(websocketSession_.getId());
+		return LOOKUP_MAP.size();
+	}
+	
+	public Collection<Lookup> getLookups() {
+		return LOOKUP_MAP.values();
+	}
+	
+		
+	public Session getSession(final String sessionId_) {
+		final Lookup lookup = LOOKUP_MAP.get(sessionId_);
+		return (lookup==null) ? null : lookup.getSession();
+	}
+	
 
+	private final Map<String,Lookup> LOOKUP_MAP = 
+			new ConcurrentHashMap<String,Lookup>();
+	
 	
 	private static final Logger logger = Logger.getLogger(WebsocketServerConfig.class);
 }
